b786e33e6689779d6cc241fa949c15c195c6521e
Arkadiy Maksimov <kam1k88@gmail.com>
2025-11-01 01:27:03 +0300

    reset: clean repository base

--- start_http.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d %~dp0
-echo [🌐] Запуск локального веб-сервера на http://localhost:5500
-start "" python -m http.server 5500
-timeout /t 2 >nul
-start "" http://localhost:5500/index.html
