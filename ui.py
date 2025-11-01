2578e9f8797ef9438aa86047faf79df91d94d066
Arkadiy Maksimov <kam1k88@gmail.com>
2025-11-01 23:20:14 +0300

    v1.1

--- ui.py
+++ ui.py
@@ -2,9 +2,30 @@ import streamlit as st
 import asyncio
 from rag_main import answer
 
+# === Настройки страницы ===
+st.set_page_config(
+    page_title="GOST1k",
+    layout="wide",  # widescreen
+    initial_sidebar_state="collapsed"
+)
+
 st.title("GOST1k")
 
-query = st.text_input("Введите запрос:")
-if query:
+# === Поле ввода ===
+query = st.text_area(
+    "Введите запрос:",
+    placeholder="""Например:
+- Пример письма руководителю ТК 260
+- Какие требования предъявляются к защите КИИ?
+- Перечисли стандарты по управлению уязвимостями
+- Сравни ГОСТ Р 57580 и СТО БР ИББС
+- Какие документы ФСТЭК регулируют категорирование?
+- Как ФЗ-152 связан с ГОСТ Р 56939?
+""",
+    height=200
+)
+
+# === Обработка ===
+if query.strip():
     ans = asyncio.run(answer(query))
     st.markdown(ans)
